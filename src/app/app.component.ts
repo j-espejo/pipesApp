d438569160ef04a7147a68f7d1a819095f567e41
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-11 17:20:56 -0400

    Intro Pipes en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'jorge espejo';
+  valor: number = 1000;
+
+  obj = {
+    nombre: 'andres',
+  };
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
