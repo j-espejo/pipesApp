1bb101b6ae383ad0a79f44923982fbfcd3bd5eaf
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-12 13:56:52 -0400

    PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 
 //Modulos personalizados
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
