79bfe9d9ec6b9bd79bf5b1cc026a219362acc1f8
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-13 13:58:23 -0400

    i18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,11 +6,21 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NoComunesComponent {
+  //i18nSelect
   nombre: string = 'Jorge';
   genero: string = 'masculino';
 
   invitacionMap = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
+  };
+
+  //i18nPlural
+  clientes: string[] = ['Lauras', 'Jorge', 'Antonia', 'Rosa'];
+  clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    other: 'tenemos # clientes esperando.',
   };
 }
