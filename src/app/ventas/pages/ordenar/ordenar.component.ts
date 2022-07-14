1e719d175e52f32361d7249d64672b6059e38fd9
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-14 10:23:33 -0400

    Llenar PrimeTable con data

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styles: [],
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
   enMayusculas: boolean = true;
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Flash',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 
   cambiar() {
     this.enMayusculas = !this.enMayusculas;
   }
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
